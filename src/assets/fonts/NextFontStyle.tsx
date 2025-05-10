0c1481c7e4f6105a54d3683c3b4373959f84f055
Supuky <saito.hajime0112@gmail.com>
2025-05-10 21:09:32 +0900

    feat: next font

---
+++ src/assets/fonts/NextFontStyle.tsx
@@ -0,0 +1,45 @@
+import { Inter, Noto_Sans_JP, Noto_Sans_Mono } from 'next/font/google';
+
+const inter = Inter({
+  weight: ['700'],
+  style: 'normal',
+  display: 'block',
+  subsets: ['latin'],
+  fallback: [],
+});
+
+const notoSansJp = Noto_Sans_JP({
+  weight: ['400', '500', '700'],
+  style: 'normal',
+  display: 'block',
+  subsets: ['latin'],
+  fallback: [],
+});
+
+const notoSansMono = Noto_Sans_Mono({
+  weight: ['400', '500', '700'],
+  style: 'normal',
+  display: 'block',
+  subsets: ['latin'],
+  fallback: [],
+});
+
+export function NextFontStyle() {
+  return (
+    <style
+      jsx
+      global
+    >
+      {`
+        :root {
+          --font-inter: ${inter.style.fontFamily};
+          --font-noto-sans-jp: ${notoSansJp.style.fontFamily};
+          --font-noto-sans-mono: ${notoSansMono.style.fontFamily};
+        }
+        html {
+          font-family: var(--font-noto-sans-jp), sans-serif;
+        }
+      `}
+    </style>
+  );
+}
