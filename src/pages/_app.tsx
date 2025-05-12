6d1601efca4fea0b059f84e776c4f868518a939e
Supuky <saito.hajime0112@gmail.com>
2025-05-12 22:20:40 +0900

    feat: add type

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
-import "@/styles/globals.css";
-import type { AppProps } from "next/app";
+import { NextFontStyle } from '@/assets/fonts/NextFontStyle';
+import '@/assets/styles/globals.scss';
+import type { NextPage } from 'next';
+import type { AppProps } from 'next/app';
+import type { ReactElement, ReactNode } from 'react';
+import { SWRConfig } from 'swr';
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = object, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement, props: P) => ReactNode;
+};
+
+type AppPropsWithLayout<P = object> = AppProps<P> & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  // const getLayout = Component.getLayout ?? ((page) => {
+  //   return <SiteLayout>{page}</SiteLayout>
+  // });
+  return (
+    <SWRConfig>
+      <NextFontStyle />
+    </SWRConfig>
+  );
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Head, Html, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang='ja'>
       <Head />
       <body>
+        {/* googletagmaneger  */}
         <Main />
         <NextScript />
       </body>
