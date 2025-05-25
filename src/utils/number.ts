940d721a71459ec0458d67500ee152621ff65947
Supuky <saito.hajime0112@gmail.com>
2025-05-25 15:33:56 +0900

    feat: util number

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -7,6 +7,11 @@ export const toNumber = (value: string) => {
   return Number(value);
 };
 
+/**
+ * 数値が0以上かどうかを判定する関数
+ * @param value - 対象の数値
+ * @returns 0以上ならtrue、そうでなければfalse
+ */
 export const isNumericString = (value: string): value is string => {
   return /^[0-9]+$/.test(value);
 };
