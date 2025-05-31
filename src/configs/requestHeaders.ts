1a1eb1e5bae024395ca17e2d4a860d321db0121d
Supuky <saito.hajime0112@gmail.com>
2025-05-31 10:03:00 +0900

    feat: create requestHeader

---
+++ src/configs/requestHeaders.ts
@@ -0,0 +1,10 @@
+export const IS_LOGIN_REQUEST_HEADER = 'x-is-login';
+export const ACCESS_TOKEN_REQUEST_HEADER = 'x-access-token';
+export const REFRESH_TOKEN_REQUEST_HEADER = 'x-refresh-token';
+export const USER_TYPE_REQUEST_HEADER = 'x-user-type';
+export const ARGEEMENT_STATUS_REQUEST_HEADER = 'x-agreement-status';
+export const GEGISTER_STATUS_REQUEST_HEADER = 'x-register-status';
+export const FIRST_STEP_REGISTER_STATUS_REQUEST_HEADER =
+  'x-first-step-register-status';
+export const SECOND_STEP_REGISTER_STATUS_REQUEST_HEADER =
+  'x-second-step-register-status';
