873770fed16cfa01de40bbe8af35252e22fdd768
Supuky <saito.hajime0112@gmail.com>
2025-06-06 21:58:52 +0900

    feat: add JSDoc comments for BroadcastChannel utilities and custom hook

--- src/hooks/webApi/broadcastChannel/useBroadcastChannel.ts
+++ src/hooks/webApi/broadcastChannel/useBroadcastChannel.ts
@@ -1,6 +1,11 @@
 import { createBroadcastChannelStore } from '@/hooks/webApi/broadcastChannel/store';
 import { useState, useSyncExternalStore } from 'react';
 
+/**
+ * BroadcastChannel を利用してメッセージを送受信するためのカスタムフック。
+ * @param channel チャンネル名
+ * @returns メッセージと送信関数を含むオブジェクト
+ */
 export const useBroadcastChannel = <T>(channel: string) => {
   const [
     {
