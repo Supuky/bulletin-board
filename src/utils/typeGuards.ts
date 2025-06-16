400269435d0c7f5fa9a1b4dbd4633fdf04ddbfdd
Supuky <saito.hajime0112@gmail.com>
2025-06-16 22:30:15 +0900

    feat: add isJson function

--- src/utils/typeGuards.ts
+++ src/utils/typeGuards.ts
@@ -24,3 +24,21 @@ export const isNull = (value: unknown): value is null => {
 export const isExist = <T>(value: T): value is NonNullable<T> => {
   return !isUndefined(value) && !isNull(value);
 };
+
+/**
+ * 値がJSON形式の文字列かどうかを判定する関数
+ * @param value - 変換対象の値
+ * @returns 値がJSON形式の文字列であればtrue、そうでなければfalse
+ */
+export const isJson = (value: unknown): value is string => {
+  if (typeof value !== 'string') {
+    return false;
+  }
+
+  try {
+    JSON.parse(value);
+    return true;
+  } catch {
+    return false;
+  }
+};
