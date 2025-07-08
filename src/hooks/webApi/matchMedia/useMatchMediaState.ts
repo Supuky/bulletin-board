9f424216a7b1cb02a4274d61d8d258c67db099e2
Supuky <saito.hajime0112@gmail.com>
2025-07-08 22:26:18 +0900

    feat: add jsdocs

--- src/hooks/webApi/matchMedia/useMatchMediaState.ts
+++ src/hooks/webApi/matchMedia/useMatchMediaState.ts
@@ -1,6 +1,11 @@
 import { createMatchMediaStateStore } from '@/hooks/webApi/matchMedia/store';
 import { useMemo, useSyncExternalStore } from 'react';
 
+/**
+ * メディアクエリの状態を監視するカスタムフック。
+ *
+ * @param query - 監視するメディアクエリの文字列。
+ */
 export const useMatchMediaState = (query: string) => {
   const {
     subscribeMatchMedia,
--- src/hooks/webApi/mutationObserver/useMutationObserverEffect.ts
+++ src/hooks/webApi/mutationObserver/useMutationObserverEffect.ts
@@ -2,6 +2,12 @@ import { isNull } from '@/utils/typeGuards';
 import type { RefObject } from 'react';
 import { useEffect } from 'react';
 
+/**
+ * カスタムフックで、ターゲット要素の変更を監視します。
+ * @param targetRef - 監視対象となる要素へのRef。
+ * @param callback - 変更が発生したときに呼び出されるコールバック関数。
+ * @param options - MutationObserverのオプション。
+ */
 export const useMutationObserverEffect = <T extends RefObject<HTMLElement>>(
   targetRef: T,
   callback: MutationCallback,
