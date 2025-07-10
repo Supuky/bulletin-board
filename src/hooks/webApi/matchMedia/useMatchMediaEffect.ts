10a19af97fcb2b7c15d6af122667ccb1ad4d8bb6
Supuky <saito.hajime0112@gmail.com>
2025-07-10 21:44:35 +0900

    feat: add jsdoc

--- src/hooks/webApi/matchMedia/useMatchMediaEffect.ts
+++ src/hooks/webApi/matchMedia/useMatchMediaEffect.ts
@@ -1,5 +1,10 @@
 import { useEffect } from 'react';
 
+/**
+ * カスタムフックで、メディアクエリの変化を監視します。
+ * @param query - 監視するメディアクエリの文字列。
+ * @param callback - メディアクエリの状態が変化したときに呼び出されるコールバック関数。
+ */
 export const useMatchMediaEffect = (
   query: string,
   callback: (e: MediaQueryListEvent) => void,
